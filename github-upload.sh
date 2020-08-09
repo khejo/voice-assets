027fce009644613b9b3b91de628dd7e4e116f2f8
rostegg <rostik.nikolaenko@gmail.com>
2020-08-09 22:06:58 +0300

    🧑‍🌾

--- github-upload.sh
+++ github-upload.sh
@@ -1,1 +1,11 @@
+export $(egrep -v '^#' .env | xargs)
+
 ENDPOINT="https://api.github.com/repos/khejo/voice-assets-source/releases"
+: '
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token ${GITHUB_AUTH_TOKEN}" \
+  $ENDPOINT \
+  -d '{"tag_name":"voice-assets-release"}'
+'
